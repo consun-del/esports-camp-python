5b67b8c4fd8aa714806217208555d2339c990e40
Raphael Souto Brandão <raphaelbrandao277353@gmail.com>
2026-09-22 20:46:29 -0300

    Versão inicial da interface e introdução ao banco de dados em Py.

---
+++ TrabalhoDia22-09.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+
+usuario_logado = None
+
+banco_de_dados = {}
+
+def limpar_janela():     
+    elementos = app.winfo_children()
+    
+    for item in elementos:
+        item.destroy()
+
+def criar_botao_limpar(tela_atual):
+    if tela_atual != "menu":
+       
+        botao_retornar_menu = tk.Button(app, text="Retornar ao Menu", command=abrir_menu)
+        botao_retornar_menu.pack(pady=50)
+
+def validar_login ():
+    entrada_usuario.get()
+    entrada_senha.get()
+
+def tela_login ():
+    limpar_janela()
+    
+    label01 = tk.Label(app, text="Bem-Vindo ao E-Sports Camp")
+    label01.pack(pady=50)
+    
+    label02 = tk.Label(app, text="Nome de Usuário: ")
+    label02.pack()
+    
+    global entrada_usuario = tk.Entry(app)
+    entrada_usuario.pack(pady=50)
+    
+    
+    label03 = tk.Label(app, text="Insira a Senha: ")
+    label03.pack()
+    global entrada_senha = tk.Entry(app, show="*")
+    entrada_senha.pack(pady=50)
+    
+    botao_entrada = tk.Button(app, text="Entrar")
+    botao_entrada.pack(pady=50)
+      criar_botao_limpar("TelaLogin") # Apenas teste.
+
+def abrir_menu():
+    limpar_janela()
+   
+    text_01 = tk.Label(app, text="MENU DE SELEÇÃO")
+    text_01.pack(pady=25)
+    
+    # Botão para cadastros
+    botao_cadastrar = tk.Button(app, text="Cadastrar", command=janela_cadastrar)
+    botao_cadastrar.pack(pady=25)
+    
+    # Botão para acessar um cadastro.
+    botao_acessarcadastro = tk.Button(app, text="Acessar Cadastro", command=acessar_cadastro)
+    botao_acessarcadastro.pack(pady=25)
+    
+    # Botão para acessar lista de participantes.
+    botao_participantes = tk.Button(app, text="Acessar Participantes")
+    botao_participantes.pack(pady=25)
+
+    criar_botao_limpar("menu")
+
+def janela_cadastrar():
+    limpar_janela()
+    
+    label = tk.Label(app, text="ACESSAR CADASTRO")
+    label.pack(pady=25)
+    
+    criar_botao_limpar("cadastrar")
+    
+def acessar_cadastro():
+    limpar_janela()
+
+    label = tk.Label(app, text="CADASTROS")
+    label.pack(pady=25)
+    
+    criar_botao_limpar("acessarcadastro")
+
+app = tk.Tk()
+app.title("E-SPORTS CAMP")
+
+tela_login()
+
+app.mainloop()
